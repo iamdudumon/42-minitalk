0fbbe649f22c47cd63ad9e6de551dd75d79cba03
iamdudumon <kksy4511@naver.com>
2024-05-26 21:37:25 +0900

    feat: str -> binary str로 변형 후 signal 전송 구현

---
+++ client/send_msg.c
@@ -0,0 +1,35 @@
+#include "ft_client.h"
+
+void    send_signal(int pid, char *binary)
+{
+    while (*binary)
+    {
+        if (*binary == '0')
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        usleep(1000);
+        binary++;
+    }
+}
+
+void    error_handler()
+{
+
+}
+
+
+void    send_msg(int pid, char *msg)
+{
+    char    *binary;
+    
+    while (*msg != '\0')
+    {
+        binary = to_binary(*msg);
+        if (!binary)
+            error_handler();
+        send_signal(pid, binary);
+        free(binary);
+        msg++;
+    }
+}
