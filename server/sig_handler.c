fa1f478b47d25f376258eb1be1e57e402bbda334
iamdudumon <kksy4511@naver.com>
2024-05-28 14:39:37 +0900

    feat: buf가 full일 때 flush 기능 추가

--- server/sig_handler.c
+++ server/sig_handler.c
@@ -14,17 +14,30 @@
 
 extern t_msg    msg;
 
+void    flush_buffer()
+{
+    if (msg.seg_no == 0)
+        ft_printf("client: ");
+    ft_printf("%s\n", msg.buf);
+    ft_memset(msg.buf, 0, BUFFER_SIZE);
+    msg.buf_len = 0;
+    msg.seg_no++;
+}
+
 void    store_binary()
 {
     msg.binary[CODE_SIZE] = 0;
     msg.buf[msg.buf_len++] = to_char(msg.binary);
-    msg.bin_len = 0;
     ft_memset(msg.binary, 0, CODE_SIZE + 1);
+    msg.bin_len = 0;
+    if (msg.buf_len == BUFFER_SIZE)
+        flush_buffer();
     if (msg.buf[msg.buf_len - 1] == '\0')
     {
-        ft_printf("cleint: %s\n", msg.buf);
+        ft_printf("client: %s\n", msg.buf);
         ft_memset(msg.buf, 0, BUFFER_SIZE);
         msg.buf_len = 0;
+        msg.seg_no = 0;
     }
 }
 
