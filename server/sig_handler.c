f0f48ac3e3a6a05e8523eee2ac3966355f9c06fd
iamdudumon <kksy4511@naver.com>
2024-06-11 20:33:02 +0900

    refactor: store_binary 함수와 sigusr_handler 함수 병합

--- server/sig_handler.c
+++ server/sig_handler.c
@@ -15,28 +15,7 @@
 
 extern t_msg	g_msg;
 
-void	store_binary(char ch)
-{
-	g_msg.buf[g_msg.len++] = ch;
-	if (g_msg.buf[g_msg.len - 1] == '\0')
-	{
-		ft_printf("client(%d): %s\n", g_msg.clt_pid, g_msg.buf);
-		init_msg(0, sigack_hadler);
-	}
-}
-
-void	sigusr_controler(int signo, siginfo_t *info, void* context)
-{
-	if (g_msg.clt_pid == info->si_pid)
-	{
-		sigusr_handler(signo);
-		kill(info->si_pid, SIGUSR1);
-	}
-	else
-		kill(info->si_pid, SIGUSR2);
-}
-
-void	sigusr_handler(int signo)
+void	store_binary(int signo)
 {
 	static char	ch;
 	static int	len;
@@ -46,10 +25,26 @@ void	sigusr_handler(int signo)
 	len++;
 	if (len == CODE_SIZE)
 	{
-		store_binary(ch);
+		g_msg.buf[g_msg.len++] = ch;
+		if (g_msg.buf[g_msg.len - 1] == '\0')
+		{
+			ft_printf("client(%d): %s\n", g_msg.clt_pid, g_msg.buf);
+			init_msg(0, sigack_hadler);
+		}
 		ch = 0;
 		len = 0;
 	}
+}
+
+void	sigusr_controler(int signo, siginfo_t *info, void* context)
+{
+	if (g_msg.clt_pid == info->si_pid)
+	{
+		store_binary(signo);
+		kill(info->si_pid, signo);
+	}
+	else
+		kill(info->si_pid, SIGUSR2);
 }
 
 void	sigack_hadler(int signo, siginfo_t *info, void* context)
