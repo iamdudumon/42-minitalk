688eb9e426cb703628d7692b3632cb63a0993aa5
iamdudumon <kksy4511@naver.com>
2024-06-20 13:59:42 +0900

    refactor: t_msg 구조 sended, recevied 변수 삭제

--- server/ft_server.h
+++ server/ft_server.h
@@ -22,11 +22,13 @@ typedef struct	s_msg
 	char				buf[BUFFER_SIZE];
 	int					len;
 	int					clt_pid;
-	int					recevied;
-	struct sigaction	handler;
+	// int					sended;
+	// int					recevied;
+	struct sigaction	sa;
 }				t_msg;
 
 void	sigack_hadler(int signo, siginfo_t *info, void* context);
 void	init_msg(int clt_pid, void (*handler)(int, siginfo_t*, void*));
+// void	ft_kill(int pid, int signo);
 
 #endif
--- server/utils.c
+++ server/utils.c
@@ -19,9 +19,10 @@ void init_msg(int clt_pid, void (*handler)(int, siginfo_t *, void *))
 	ft_memset(g_msg.buf, 0, sizeof(char) * BUFFER_SIZE);
 	g_msg.len = 0;
 	g_msg.clt_pid = clt_pid;
-	g_msg.recevied = 0;
-	g_msg.handler.sa_flags = SA_SIGINFO;
-	g_msg.handler.sa_sigaction = *handler;
-	sigaction(SIGUSR1, &g_msg.handler, NULL);
-	sigaction(SIGUSR2, &g_msg.handler, NULL);
+	// g_msg.sended = 0;
+	// g_msg.recevied = 0;
+	g_msg.sa.sa_flags = SA_SIGINFO;
+	g_msg.sa.sa_sigaction = *handler;
+	sigaction(SIGUSR1, &g_msg.sa, NULL);
+	sigaction(SIGUSR2, &g_msg.sa, NULL);
 }
